6ec1553aea43b5bc07c3541e10fe4429bd1c852c
Adrian <Adrian.Essig@outlook.com>
2022-03-25 16:43:02 +0100

    Populating TableView works TODO:  - clean up code  - change fields of SongDTOs?  - change fields of AlbumDTO, add artists  - generally add artists to objects

--- src/main/java/at/fhv/musicshopfx/MusicOverviewController.java
+++ src/main/java/at/fhv/musicshopfx/MusicOverviewController.java
@@ -1,8 +1,17 @@
 package at.fhv.musicshopfx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import sharedrmi.application.dto.AlbumDTO;
+import sharedrmi.application.dto.SongDTO;
+
+import java.util.List;
+import java.util.Set;
 
 public class MusicOverviewController {
     @FXML
@@ -17,6 +26,14 @@ public class MusicOverviewController {
     private TextField stockTextField;
     @FXML
     private TextField priceTextField;
+    @FXML
+    private TableView songsTableView;
+    @FXML
+    private TableColumn titleCol;
+    @FXML
+    private TableColumn genreCol;
+    @FXML
+    private TableColumn artistCol;
 
 
     public void setData(AlbumDTO albumDTO){
@@ -25,6 +42,17 @@ public class MusicOverviewController {
         releaseDateTextField.setText(albumDTO.getReleaseDate().toString());
         stockTextField.setText(Integer.toString(albumDTO.getStock()));
         priceTextField.setText(albumDTO.getPrice().toString());
+
+
+        Set<SongDTO> songs = albumDTO.getSongs();
+
+        ObservableList<SongDTO> songDTOs = FXCollections.observableArrayList(songs);
+
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        genreCol.setCellValueFactory(new PropertyValueFactory<>("genre"));
+        artistCol.setCellValueFactory(new PropertyValueFactory<>("artists"));
+
+        songsTableView.setItems(songDTOs);
 
     }
 
