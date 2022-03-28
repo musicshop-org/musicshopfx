b3dc9f1e329e749b446a8f534f02bfa1c26d594a
Fabian Egartner <fabian.egartner@hotmail.com>
2022-03-28 18:35:09 +0200

    shared rmi updated + dev merged

--- src/main/java/sharedrmi/application/dto/AlbumDTO.java
+++ src/main/java/sharedrmi/application/dto/AlbumDTO.java
@@ -9,8 +9,8 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Set;
-import sharedrmi.domain.enums.*;
-import sharedrmi.domain.valueobjects.*;
+import sharedrmi.domain.enums.MediumType;
+import sharedrmi.domain.valueobjects.AlbumId;
 
 public class AlbumDTO implements Serializable {
     private final String title;
@@ -22,15 +22,19 @@ public class AlbumDTO implements Serializable {
     private final String label;
     private final Set<SongDTO> songs;
 
-    public AlbumDTO(String var1, BigDecimal var2, int var3, MediumType var4, LocalDate var5, AlbumId var6, String var7, Set<SongDTO> var8) {
-        this.title = var1;
-        this.price = var2;
-        this.stock = var3;
-        this.mediumType = var4;
-        this.releaseDate = var5;
-        this.albumId = var6;
-        this.label = var7;
-        this.songs = var8;
+    public AlbumDTO(String title, BigDecimal price, int stock, MediumType mediumType, LocalDate releaseDate, AlbumId albumId, String label, Set<SongDTO> songs) {
+        this.title = title;
+        this.price = price;
+        this.stock = stock;
+        this.mediumType = mediumType;
+        this.releaseDate = releaseDate;
+        this.albumId = albumId;
+        this.label = label;
+        this.songs = songs;
+    }
+
+    public static AlbumDTO.AlbumDTOBuilder builder() {
+        return new AlbumDTO.AlbumDTOBuilder();
     }
 
     public String getTitle() {
@@ -63,5 +67,67 @@ public class AlbumDTO implements Serializable {
 
     public Set<SongDTO> getSongs() {
         return this.songs;
+    }
+
+    public static class AlbumDTOBuilder {
+        private String title;
+        private BigDecimal price;
+        private int stock;
+        private MediumType mediumType;
+        private LocalDate releaseDate;
+        private AlbumId albumId;
+        private String label;
+        private Set<SongDTO> songs;
+
+        AlbumDTOBuilder() {
+        }
+
+        public AlbumDTO.AlbumDTOBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public AlbumDTO.AlbumDTOBuilder price(BigDecimal price) {
+            this.price = price;
+            return this;
+        }
+
+        public AlbumDTO.AlbumDTOBuilder stock(int stock) {
+            this.stock = stock;
+            return this;
+        }
+
+        public AlbumDTO.AlbumDTOBuilder mediumType(MediumType mediumType) {
+            this.mediumType = mediumType;
+            return this;
+        }
+
+        public AlbumDTO.AlbumDTOBuilder releaseDate(LocalDate releaseDate) {
+            this.releaseDate = releaseDate;
+            return this;
+        }
+
+        public AlbumDTO.AlbumDTOBuilder albumId(AlbumId albumId) {
+            this.albumId = albumId;
+            return this;
+        }
+
+        public AlbumDTO.AlbumDTOBuilder label(String label) {
+            this.label = label;
+            return this;
+        }
+
+        public AlbumDTO.AlbumDTOBuilder songs(Set<SongDTO> songs) {
+            this.songs = songs;
+            return this;
+        }
+
+        public AlbumDTO build() {
+            return new AlbumDTO(this.title, this.price, this.stock, this.mediumType, this.releaseDate, this.albumId, this.label, this.songs);
+        }
+
+        public String toString() {
+            return "AlbumDTO.AlbumDTOBuilder(title=" + this.title + ", price=" + this.price + ", stock=" + this.stock + ", mediumType=" + this.mediumType + ", releaseDate=" + this.releaseDate + ", albumId=" + this.albumId + ", label=" + this.label + ", songs=" + this.songs + ")";
+        }
     }
 }
