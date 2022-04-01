c2d76cbd2e8800da3d53e02fdb92b391e636c9be
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-02 00:10:00 +0200

    added Role

--- src/main/java/sharedrmi/communication/rmi/RMIController.java
+++ src/main/java/sharedrmi/communication/rmi/RMIController.java
@@ -1,10 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package sharedrmi.communication.rmi;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
 import sharedrmi.application.api.ProductService;
 import sharedrmi.application.api.ShoppingCartService;
-
-import java.rmi.Remote;
+import sharedrmi.domain.valueobjects.Role;
 
 public interface RMIController extends ProductService, ShoppingCartService, Remote {
+    List<Role> getRoles() throws RemoteException;
 
+    String getUsername() throws RemoteException;
 }
