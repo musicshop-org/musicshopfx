3976eddfc84cb98d6a3c355898c47187e84ed3ba
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-04 15:58:34 +0200

    branch creation

--- src/main/java/at/fhv/musicshopfx/UserDataStore.java
+++ src/main/java/at/fhv/musicshopfx/UserDataStore.java
@@ -1,4 +1,19 @@
 package at.fhv.musicshopfx;
 
 public class UserDataStore {
+
+    private static UserDataStore instance;
+    private static List<Role>
+
+    public static UserDataStore getInstance() {
+        if (null == UserDataStore.instance) {
+            new UserDataStore();
+        }
+
+        return UserDataStore.instance;
+    }
+
+    private UserDataStore() {
+        UserDataStore.instance = this;
+    }
 }
