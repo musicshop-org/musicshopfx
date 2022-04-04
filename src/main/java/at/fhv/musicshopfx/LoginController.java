f87b5db6652b1334aa63b05529a9e0fab9ed4fcc
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-04 20:06:47 +0200

    Login UI finished

---
+++ src/main/java/at/fhv/musicshopfx/LoginController.java
@@ -0,0 +1,30 @@
+package at.fhv.musicshopfx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label loginFailedLabel;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private PasswordField usernameTextField;
+
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/at/fhv/musicshopfx/MainApp.java
+++ src/main/java/at/fhv/musicshopfx/MainApp.java
@@ -10,21 +10,21 @@ import java.io.IOException;
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage stage) throws NotLoggedInException, IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("musicSearch-view.fxml"));
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
 
         stage.setTitle("Music Shop");
         stage.setScene(scene);
         stage.show();
 
-        final String USERNAME = "essiga";
-        final String PASSWORD = "password01";
+//        final String USERNAME = "essiga";
+//        final String PASSWORD = "password01";
 
 //        final String USERNAME = "prescher";
 //        final String PASSWORD = "password02";
 
-        System.out.println(SessionManager.login(USERNAME, PASSWORD));
+//        System.out.println(SessionManager.login(USERNAME, PASSWORD));
 //        SessionManager sessionManager = SessionManager.getInstance();
 //        System.out.println(sessionManager.getRMIController().getUsername());
 //        System.out.println(sessionManager.getRMIController().getRoles());
