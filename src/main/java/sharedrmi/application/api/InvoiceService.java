e00b352aac7e40d249ca30088f6d35e62b7c45fd
Marco Prescher <m.prescher@gantner-instruments.com>
2022-04-10 09:57:35 +0200

    solved merge conflicts

--- src/main/java/sharedrmi/application/api/InvoiceService.java
+++ src/main/java/sharedrmi/application/api/InvoiceService.java
@@ -1,16 +1,20 @@
-package sharedrmi.application.api;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
-import sharedrmi.application.dto.InvoiceDTO;
-import sharedrmi.domain.valueobjects.InvoiceId;
+package sharedrmi.application.api;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.Optional;
+import sharedrmi.application.dto.InvoiceDTO;
+import sharedrmi.application.dto.InvoiceLineItemDTO;
+import sharedrmi.domain.valueobjects.InvoiceId;
 
 public interface InvoiceService extends Remote {
+    InvoiceDTO findInvoiceById(InvoiceId var1) throws RemoteException, Exception;
 
-    InvoiceDTO findInvoiceById(InvoiceId invoiceId) throws RemoteException;
+    void createInvoice(InvoiceDTO var1) throws RemoteException;
 
-    void createInvoice(InvoiceDTO invoiceDTO) throws RemoteException;
-
+    void returnInvoiceLineItem(InvoiceId var1, InvoiceLineItemDTO var2, int var3) throws RemoteException, Exception;
 }
