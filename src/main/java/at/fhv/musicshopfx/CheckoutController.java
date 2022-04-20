3a73507bdf450a5ffbc07b7b8a5b99aaea21cf72
Fabian Egartner <fabian.egartner@hotmail.com>
2022-04-20 18:16:07 +0200

    logout implemented in every fxml

--- src/main/java/at/fhv/musicshopfx/CheckoutController.java
+++ src/main/java/at/fhv/musicshopfx/CheckoutController.java
@@ -150,6 +150,20 @@ public class CheckoutController {
     }
 
     @FXML
+    protected void logoutSymbolClicked(MouseEvent e) throws IOException {
+        if (e.isPrimaryButtonDown()) {
+            try {
+                SessionManager.logout();
+            } catch (NotLoggedInException ex) {
+                ex.printStackTrace();
+                return;
+            }
+
+            sceneSwitcher.switchSceneToLoginView(e);
+        }
+    }
+
+    @FXML
     protected void customerSearchButtonClicked() {
         try {
 
