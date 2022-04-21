601e68dd0a058e663d127a219718e1a8dc86ed08
mme8372 <matthias.meier@students.fhv.at>
2022-04-21 15:29:08 +0200

    nothing works and i am sad

--- src/main/java/sharedrmi/application/api/MessageConsumerService.java
+++ src/main/java/sharedrmi/application/api/MessageConsumerService.java
@@ -3,12 +3,14 @@ package sharedrmi.application.api;
 import sharedrmi.application.dto.MessageDTO;
 import sharedrmi.application.exceptions.NoMessagesFoundException;
 
+import javax.jms.JMSException;
+import java.io.IOException;
 import java.util.List;
 
 public interface MessageConsumerService {
 
     List<MessageDTO> getMessagesFromAllSubscribedTopics() throws NoMessagesFoundException;
 
-    List<MessageDTO> getMessagesFromSubscribedTopic(String topic) throws NoMessagesFoundException;
+    List<MessageDTO> getMessagesFromSubscribedTopic(String topic) throws NoMessagesFoundException, IOException, JMSException;
 
 }
