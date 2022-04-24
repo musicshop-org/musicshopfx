eec2cc0583571ab306c0b5abe1e321d471578810
Adrian <adrian.essig@outlook.com>
2022-04-24 11:35:02 +0200

    merged with dev

--- src/main/java/sharedrmi/application/api/UserService.java
+++ src/main/java/sharedrmi/application/api/UserService.java
@@ -15,6 +15,5 @@ import java.util.List;
 public interface UserService extends Remote {
     List<String> getAllTopics() throws RemoteException;
     List<String> getSubscribedTopicsForUser(String var1) throws RemoteException;
-
-    void changeLastViewed(String var1, LocalDateTime var2) throws UserNotFoundException;
+    void changeLastViewed(String var1, LocalDateTime var2) throws UserNotFoundException, RemoteException;
 }
